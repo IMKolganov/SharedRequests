ca43a83f007c2f19703823daf3c4dc8b1c2c289e
Ivan Kolganov <ikolganov@whitebox.ru>
2024-11-18 02:00:18 +0200

    init project

---
+++ SmartGarden/Models/TemperatureHumidityRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace SharedRequests.SmartGarden.Models;
+
+public class TemperatureHumidityRequest
+{
+    public TemperatureHumidityRequest() { }
+
+    public TemperatureHumidityRequest(Guid requestId)
+    {
+        RequestId = requestId;
+    }
+
+    public Guid RequestId { get; set; }
+    public bool UseRandomValuesFotTest { get; set; }
+    public DateTime RequestDate { get; set; }
+    public int SensorId { get; set; }
+}
